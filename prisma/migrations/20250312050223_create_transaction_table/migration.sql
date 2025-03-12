db5ca70bd58ae27b1ad3cb66b62cb92b860bee5b
qolbudr <qolbu.dzikru30@gmail.com>
2025-03-12 12:02:46 +0700

    feat-create transaction table

---
+++ prisma/migrations/20250312050223_create_transaction_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `productId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
